7572bb7438e054c4c3df606622ab6aebe9bec1c4
Hamadullah12 <hamadfms05@gmail.com>
2024-06-04 12:39:52 +0500

    added files

---
+++ JS01.js
@@ -0,0 +1,15 @@
+//console.log is used to print something on console
+
+// what is string
+
+// something that is enclosed in single qoutes 'hamad' or double quotes "hamad" so the enclosed words are string
+
+console.log("hello world");
+
+//variable is used to store some information that can be use and change in future/later
+
+// var is the keyword for variable
+
+// we have to declare a variable so in the below line the declare variable is 'first name'
+// var > is the keyword , firstname is the > declare ,and hamad is the > string
+var firstname = "hamad";
