daffa20a2de684e0d4186dddb7ca8360a38de867
Hamadullah12 <hamadfms05@gmail.com>
2024-06-04 21:25:09 +0500

    add files

--- JS01.js
+++ JS01.js
@@ -1,3 +1,7 @@
+"use strict";
+
+// use strict safe us from making mistakes
+
 //console.log is used to print something on console
 
 // what is string
@@ -12,4 +16,27 @@ console.log("hello world");
 
 // we have to declare a variable so in the below line the declare variable is 'first name'
 // var > is the keyword , firstname is the > declare ,and hamad is the > string
+
 var firstname = "hamad";
+
+// mow to implemnt the var keyword we will type "Console.log"firstname";"
+console.log(firstname);
+
+// now change value
+
+firstname = "ahmed ali";
+
+console.log(firstname);
+
+var lastname = "ullah";
+
+console.log(lastname);
+
+// Rules for naming variable
+// never start with number
+// example    >>var 8value=8>>invalid wrong    ===   var value8=8;>>valid right
+
+var value8 = 8;
+console.log(value8 - 98);
+
+// so in the above example we see that we can add ,sub , multiply and divide with numbers
